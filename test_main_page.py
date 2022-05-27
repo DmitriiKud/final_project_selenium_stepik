77bada6ca20c37789a9b2add09a5b40178487f13
DmitriiKud <dmitrii.kudriavtcev@gmail.com>
2022-05-27 12:04:18 +0300

    Second interim commit for page object mod

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,21 @@
 import pytest
 from selenium import webdriver
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
-#выделяем откртие страницы регистрации в отдельное действие    
+# выделяем открытие страницы регистрации в отдельное действие    
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
-#получается упрощенный тест
+# получается упрощенный тест
 def test_guest_can_go_to_login_page(browser): 
-   link = "http://selenium1py.pythonanywhere.com/"
-   page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-   page.open() # открываем страницу
-   page.go_to_login_page() # выполняем метод страницы - переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open() # открываем страницу
+    page.got_to_login_page()
+    login_page = LoginPage(browser, browser.current_url) # выполняем метод страницы - переходим на страницу логина
+    login_page.should_be_login_page()
 
 # проверяем, что есть ссылка, которая ведет на логин
 def test_guest_should_see_login_link(browser):
@@ -20,6 +23,19 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # старая версия
 """def test_guest_can_go_to_login_page(browser):
